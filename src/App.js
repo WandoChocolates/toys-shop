1ee8582b310a1c53f52307d8908fa6ad95b78365
WandoChocolates <ellytitus50@gmail.com>
2021-08-18 19:38:16 +0800

    bootstrap added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
 import "./style.css";
 
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import Index from "./components/Index/Index";
 
 export default function App() {
